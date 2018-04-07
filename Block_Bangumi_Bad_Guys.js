530c3d0b11a2b7fdec584ddfa9a18e59da6123ce
gyakkun <xiaowufeixia@gmail.com>
2018-04-07 16:56:37 +0800

    Add function "RemoveCommetFromSubjectCommentDetailPage". Make refactors to odd/even handling.

--- Block_Bangumi_Bad_Guys.js
+++ Block_Bangumi_Bad_Guys.js
@@ -3,7 +3,7 @@
 // @description   You can (not) see me (bgm38).
 // @copyright     gyakkun
 // @license       MIT
-// @version       0.0.10
+// @version       0.0.12
 // @include       http://bangumi.tv/*
 // @include       http://bgm.tv/*
 // @include       https://bgm.tv/*
@@ -13,17 +13,11 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+    //'use strict';
 
     var url = window.location.pathname;
     var BLOCK_USERNAME_LIST = [];
     var BLOCK_UID_LIST = [];
-    //URLs
-    var INDEX_URL = /^\/$/;
-    var TIMELINE_URL = /\/timeline/;
-    var THREAD_URL = /\/(ep|blog|subject|group)/;
-    var CATEGORY_URL = /\/(anime|book|music|real)/;
-    var SETTINGS_URL = /^\/settings$/;
 
     try {
         if (!!localStorage["blockUsernameList"]) {
@@ -40,6 +34,14 @@
         console.error(err);
     }
 
+    //URLs
+    var INDEX_URL = /^\/$/;
+    var TIMELINE_URL = /\/timeline/;
+    var THREAD_URL = /\/(ep|blog|subject|group)/;
+    var CATEGORY_URL = /\/(anime|book|music|real)/;
+    var SETTINGS_URL = /^\/settings$/;
+
+    //Selectors
     var INDEX_TIMELINE_AVATAR_SELECTOR = "#timeline > ul > li > span > a > span";
     var INDEX_THREAD_AVATAR_SELECTOR = "div > ul > li > a > img";
     var INDEX_TIMELINE_USERNAME_SELECTOR = "#timeline > ul > li > span > a";
@@ -49,18 +51,34 @@
     var SUBJECT_DOING_USERNAME_SELECTOR = "#subjectPanelCollect > ul > li > div > a";
     var SUBJECT_INDEX_USERNAME_SELECTOR = "#subjectPanelIndex > ul > li > a";
     var SUBJECT_RATING_USERNAME_SELECTOR = "#memberUserList > li > div > strong > a";
+    var SUBJECT_COMMENT_DETAIL_PAGE_USERNAME_SELECTOR = "#comment_box > div > a";
     var CATEGORY_BLOG_USERNAME_SELECTOR = "#news_list > div > div > div.time > small.blue > a";
     var CATEGORY_THREAD_USERNAME_SELECTOR = "#columnA > div > table > tbody > tr > td > a";
 
+    //Regexes
     var USERNAME_REG = /user\/(.+)/;
     var AVATAR_UID_REG = /pic\/user\/.+\/\d+\/\d+\/\d+\/(\d+)\.jpg/;
+
+    var GetFuntionSelfName = function() {
+        return arguments.callee.name;
+    };
+
+    var GetFinalCallerName = function(functionSelf) {
+        var finalCaller = functionSelf.caller.name;
+        while (finalCaller == "" || finalCaller == "each") {
+            functionSelf = functionSelf.caller.arguments.callee;
+            finalCaller = functionSelf.caller.name;
+        }
+        return finalCaller;
+    };
 
     var RemoveByUsername = function(obj, selectorToRemove) {
         try {
             var username = obj.attr("href").match(USERNAME_REG)[1];
             if ($.inArray(username, BLOCK_USERNAME_LIST) >= 0) {
                 obj.closest(selectorToRemove).hide();
-                console.warn(username + " was removed by username.");
+                console.warn(username + " was removed by username, " +
+                    "called by function " + RemoveByUsername.caller.name);
                 return true;
             }
         } catch (err) {}
@@ -73,11 +91,38 @@
             var userId = Number(userIdNotNumber);
             if ($.inArray(userId, BLOCK_UID_LIST) >= 0) {
                 obj.closest(selectorToRemove).hide();
-                console.warn(userId + " was removed by uid.");
+                console.warn(userId + " was removed by uid." +
+                    "called by function " + RemoveByUid.caller.name);
                 return true;
             }
         } catch (err) {}
         return false;
+    };
+
+    var OddEvenClassInnerHandler = function(obj, selector, oddClass, evenClass) {
+        console.warn("Odd/even class inner handled from function " + GetFinalCallerName(OddEvenClassInnerHandler));
+        obj.find(selector).each(function() {
+            if ($(this).hasClass(oddClass)) {
+                $(this).removeClass(oddClass);
+                $(this).addClass(evenClass);
+            } else {
+                $(this).removeClass(evenClass);
+                $(this).addClass(oddClass);
+            }
+        });
+
+    };
+
+    var OddOrEvenClassOuterHandler = function(obj, selector, OddOrEvenClass) {
+        //This function only add/remove one class which is odd/even related.
+        console.warn("Odd/even class outer handled from function " + GetFinalCallerName(OddOrEvenClassOuterHandler));
+        obj.closest("selector").nextAll().each(function() {
+            if ($(this).hasClass(OddOrEvenClass)) {
+                $(this).removeClass(OddOrEvenClass);
+            } else {
+                $(this).addClass(OddOrEvenClass);
+            }
+        });
     };
 
     var RemoveUidIndexTimeline = function() {
@@ -124,12 +169,26 @@
         var that = $(this);
         var isSuccess = RemoveByUsername(that, "div.item");
         if (isSuccess) {
-            that.closest("div.item").nextAll().each(function() {
-                if ($(this).hasClass("odd")) {
-                    $(this).removeClass("odd");
-                } else {
-                    $(this).addClass("odd");
-                }
+            OddOrEvenClassOuterHandler(that, "div.item", "odd");
+        }
+    };
+
+
+    var RemoveRatingFromSubject = function() {
+        var that = $(this);
+        var isSuccess = RemoveByUsername(that, "li.user");
+        if (isSuccess) {
+            OddOrEvenClassOuterHandler(that, "li.user", "odd");
+        }
+    };
+
+    var RemoveCommetFromSubjectCommentDetailPage = function() {
+        var that = $(this);
+        var isSuccess = RemoveByUsername(that, "div.clearit");
+        if (isSuccess) {
+            that.closest("div.clearit").nextAll().each(function() {
+                OddEvenClassInnerHandler($(this), "> div[class^='text_main']", "text_main_odd", "text_main_even");
+                OddEvenClassInnerHandler($(this), "> a > span.avatarNeue", "ll", "rr");
             });
         }
     };
@@ -139,32 +198,11 @@
         var isSuccess = RemoveByUsername(that, "tr");
         if (isSuccess) {
             that.closest("tr").nextAll().each(function() {
-                $(this).find(">td").each(function() {
-                    if ($(this).hasClass("odd")) {
-                        $(this).removeClass("odd");
-                        $(this).addClass("even");
-                    } else {
-                        $(this).removeClass("even");
-                        $(this).addClass("odd");
-                    }
-                });
+                OddEvenClassInnerHandler($(this), ">td", "odd", "even");
             });
         }
     };
 
-    var RemoveRatingFromSubject = function() {
-        var that = $(this);
-        var isSuccess = RemoveByUsername(that, "li.user");
-        if (isSuccess) {
-            that.closest("li.user").nextAll().each(function() {
-                if ($(this).hasClass("odd")) {
-                    $(this).removeClass("odd");
-                } else {
-                    $(this).addClass("odd");
-                }
-            });
-        }
-    };
 
     if (url.match(TIMELINE_URL) !== null || url.match(INDEX_URL) !== null) {
         $(INDEX_TIMELINE_AVATAR_SELECTOR).each(RemoveUidIndexTimeline);
@@ -179,6 +217,7 @@
         $(SUBJECT_DOING_USERNAME_SELECTOR).each(RemoveDoingFromSubject);
         $(SUBJECT_INDEX_USERNAME_SELECTOR).each(RemoveIndexFromSubject);
         $(SUBJECT_RATING_USERNAME_SELECTOR).each(RemoveRatingFromSubject);
+        $(SUBJECT_COMMENT_DETAIL_PAGE_USERNAME_SELECTOR).each(RemoveCommetFromSubjectCommentDetailPage);
     }
 
     if (url.match(CATEGORY_URL) !== null) {
